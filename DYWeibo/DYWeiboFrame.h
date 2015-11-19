a1bf30de8ed0dd55346cb719cb2f4a645b7ff962
David.Yu <lmyu@wisorg.com>
2015-11-19 21:44:40 +0800

    add WeiboFrame for custom cell view

---
+++ DYWeibo/DYWeiboFrame.h
@@ -0,0 +1,31 @@
+//
+// Created by YuLeiming on 19/11/2015.
+// Copyright (c) 2015 David.Yu. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+
+@class NSWeibo;
+
+
+@interface DYWeiboFrame : NSObject
+
+//头像frame
+@property (nonatomic, assign) CGRect iconF;
+
+//名字frame
+@property (nonatomic, assign) CGRect nameF;
+
+//正文的frame
+@property (nonatomic, assign) CGRect introF;
+
+//行高
+@property (nonatomic, assign) CGFloat cellHeightF;
+
+//微博实体对象
+@property (nonatomic, strong) NSWeibo * weibo;
+
+@property (nonatomic, assign) CGFloat screenWidth;
+
+@end
---
+++ DYWeibo/DYWeiboFrame.m
@@ -0,0 +1,69 @@
+//
+// Created by YuLeiming on 19/11/2015.
+// Copyright (c) 2015 David.Yu. All rights reserved.
+//
+
+#import "DYWeiboFrame.h"
+#import "NSWeibos.h"
+
+
+@implementation DYWeiboFrame {
+
+}
+
+- (void)setWeibo:(NSWeibo *)weibo {
+    _weibo = weibo;
+    CGFloat padding = 10;
+
+    self.iconF = ({
+        CGFloat iconViewX = padding;
+        CGFloat iconViewY = padding;
+        CGFloat iconW = 40;
+        CGFloat iconH = 40;
+        CGRectMake(iconViewX, iconViewY, iconW, iconH);
+    });
+
+    self.nameF = ({
+        CGFloat nameLabelX = CGRectGetMaxX(_iconF) + padding;
+        CGFloat nameLabelY = padding;
+        CGSize size = [self sizeWithString:_weibo.user.name font:[UIFont systemFontOfSize:14] maxSize:CGSizeMake(_screenWidth - padding,MAXFLOAT)];
+        CGFloat nameLabelW = size.width;
+        CGFloat nameLabelH = size.height;
+        CGRectMake(nameLabelX, nameLabelY, nameLabelW, nameLabelH);
+    });
+
+    self.introF = ({
+        CGFloat introLabelX = padding;
+        CGFloat introLabelY = CGRectGetMaxY(_iconF) + padding;
+        CGSize size = [self sizeWithString:_weibo.text font:[UIFont systemFontOfSize:14] maxSize:CGSizeMake(_screenWidth - padding,MAXFLOAT)];
+        CGFloat nameLabelW = size.width;
+        CGFloat nameLabelH = size.height;
+        CGRectMake(introLabelX, introLabelY, nameLabelW, nameLabelH);
+    });
+
+    self.cellHeightF = ({
+        CGRectGetMaxY(_introF) + padding;
+    });
+
+}
+
+
+/**
+ *  计算文本的宽高
+ *
+ *  @param str     需要计算的文本
+ *  @param font    文本显示的字体
+ *  @param maxSize 文本显示的范围
+ *
+ *  @return 文本占用的真实宽高
+ */
+- (CGSize)sizeWithString:(NSString *)str font:(UIFont *)font maxSize:(CGSize)maxSize{
+    NSDictionary *dict = @{NSFontAttributeName : font};
+    // 如果将来计算的文字的范围超出了指定的范围,返回的就是指定的范围
+    // 如果将来计算的文字的范围小于指定的范围, 返回的就是真实的范围
+    CGSize size =  [str boundingRectWithSize:maxSize options:NSStringDrawingUsesLineFragmentOrigin attributes:dict context:nil].size;
+    return size;
+}
+
+
+@end
